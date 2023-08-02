75236e805b98be808e3aad86073b996d25ada209
Amx1d <A7mmd.almadii@gmail.com>
2023-08-02 21:49:04 +0300

    - Raven Changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: Row(
+          children: [],
+        ),
+      ),
     );
   }
 }
