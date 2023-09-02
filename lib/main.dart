bb33aa67225e18348907aeacaf1778f2e27b8fae
alaresof <alfaresof@gmail.com>
2023-09-02 17:18:21 +0300

    add my name

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         ),
         appBar: AppBar(
           title: Text(
-            "Git Course after editing from Obida",
+            "Git Course after editing from Obida Farhat",
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
